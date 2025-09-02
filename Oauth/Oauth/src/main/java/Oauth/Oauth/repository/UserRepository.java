e01c5323b00afc93b70ad5fb8d1f05f2b6b7cdac
이하경 <s25048@gsm.hs.kr>
2025-09-02 20:56:42 +0900

    Delete Oauth/Oauth directory

--- Oauth/Oauth/src/main/java/Oauth/Oauth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Oauth.Oauth.repository;
-
-import Oauth.Oauth.vo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByID (String username);
-}
--- Oauth/Oauth/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table `user`(
-    id varchar(20),
-    password varcher(100)
-    nickname varchar(100)
-    state char(1)
-)
